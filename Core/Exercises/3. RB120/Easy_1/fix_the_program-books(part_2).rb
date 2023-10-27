de36f892f24039b36dcc9371fca731489dc4e7f9
jontirabassi <jonathontirabassi@gmail.com>
2023-10-26 21:13:54 -0400

    OO Easy 1 Exercises

---
+++ Core/Exercises/3. RB120/Easy_1/fix_the_program-books(part_2).rb
@@ -0,0 +1,15 @@
+class Book
+
+  attr_aceesor :author, :book
+  
+
+  def to_s
+    %("#{title}", by #{author})
+  end
+end
+
+book = Book.new
+book.author = "Neil Stephenson"
+book.title = "Snow Crash"
+puts %(The author of "#{book.title}" is #{book.author}.)
+puts %(book = #{book}.)
