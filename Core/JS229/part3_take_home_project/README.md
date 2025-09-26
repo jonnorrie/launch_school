89b51f65881c0a98d45a159b2dad3f1a870eada4
jontirabassi <jonathontirabassi@gmail.com>
2025-09-26 13:35:07 -0400

    todoApp and README completed

--- Core/JS229/part3_take_home_project/README.md
+++ Core/JS229/part3_take_home_project/README.md
@@ -1,4 +1,4 @@
-Welcome to todoApp
+Welcome to todoApp.js
 
 An app for keeping track of tasks.
 
@@ -7,29 +7,58 @@ NodeJS v18.19.1,
 Ubuntu 16.6-0ubuntu0.24.04.1
 
 Requirements:
-NodeJS is required to run this application. 
+NodeJS is required to run this application.
+
+Assumptions Made During Development:
+- There is only one `todoList` created.
+- It is possible for there to be multiple todos with the same name.
+- the `todoList` object is to be prepopulated with `Todo` objects upon startup.
+- there is not user input anywhere in this application
 
 Running the app:
-This app runs in the terminal, so running the app simply requires you to navigate to the directory with the `todoApp.js` file within the terminal, and entering the command `node todoApp.js`.
+This app runs in the terminal, so running the app simply requires you to navigate to the directory with the `todoApp.js` file within the terminal, and enter the command `node todoApp.js`. The test cases provided at the end of the file will be executed and logged to the console.
 
-Upon startup of the application, the list is populated with example data.
+Upon startup of the application, the list is populated with example data. Currently it is not possible to prompt the user for input through the console.
 
-The app allows you to store a list of tasks, called "todo"s. You can then interact with the list and the items in the list in the following ways:
-- add a new todo item to the list (`todoList.addTodo()`)
+
+
+
+The `todoList`:
+
+The app allows you to store a list of tasks, called "todo"s. You can then interact with the list and the items in the list using the `todoList` object's methods:
+- add a new todo item to the list (`todoList.addTodo(object)`)
   - this method takes an object as an argument with key-value pairs for `title`, `month`, `year`, and `description`.
-  - new todos are provided with a private, unique `id` number. A todo's id cannot be changed
+  - created `todo` objects are provided with a private, unique `id` number. A todo's id cannot be changed
+
 - delete a todo item from the list (`todoList.deleteTodo()`)
   - this method takes a string as an argument, and removes the item in the list with a title that matches the input string.
   - Since the list does not restrict todos to have unique names, in the case of duplicate names, only the first instance with a matching name will be deleted, and the rest will remain.
+
 - update properties of a todo
-  - each property capable of being changed has its own method of interacting with it
-    - (`updateTitle(title, newValue)`)
-    - (`updateMonth(title, newValue)`)
-    - (`updateYear(title, newValue)`)
-    - (`updateDescription(title, newValue)`)
+  - each property capable of being changed has its own method of interacting with it. Each `update` method takes a title as a string to indicate which todo object to update, and a new string value to replace the corresponding existing value and returns a copy of the updated todo object
+    - (`updateTitle(title, newTitle)`) updates the title of a todo to the passed in `newTitle`
+    - (`updateMonth(title, newMonth)`) updates the month of a todo to the passed in `newMonth`
+    - (`updateYear(title, newYear)`) updates the year of a todo to the passed in `newYear`
+    - (`updateDescription(title, newDescription)`) updates the description of a todo to the passed in `newDescription`
     - (`completeTodo(title)`)
-  - the `id` property is not capable of being changed
+      - if the property of the todo object with a matching title is false, this will be changed to true. 
+      - if the property of the todo object with a matching title is true, this will be changed back to false. 
+  - the `id` property is not capable of being changed.
+
+- retrieve a todo object by its id (`getTodoById(idInput)`)
+  - returns only a copy of the todo object with matching id properties
 
 
-Assumptions:
-- There is only one todoList created at a time. 
+
+The `todoManager`:
+
+  A `todoManager` is included within the app to query the `todoList` in the following ways:
+- obtain an array of all the todo objects currently in the `todoList` (`todoManager.getAll()`)
+
+- obtain an array of todo objects with a `completed` property value of `true` (`todo.Manager.getCompleted()`)
+
+- obtain an array of todo objects with specific `month` and `year` properties (`todoManager.getWithinMonthYear(month, year)`)
+
+- obtain an array of todo objects with specific `month` and `year` properties AND that have a `completed` property value of `true` (`todoManager.getCompletedWithinMonthYear(month, year)`)
+
+
--- Core/JS229/part3_take_home_project/todoApp.js
+++ Core/JS229/part3_take_home_project/todoApp.js
@@ -1,46 +1,49 @@
-function Todo(title, id, month = '', year = '', description){
+class Todo{
   
-  this.id = id,
-  this.title = title,
-  this.month = month,
-  this.year = year,
-  this.description = description,
-  this.completed = false,
+  constructor(title, id, month = '', year = '', description, completed = false){
+    this.id = id,
+    this.title = title,
+    this.month = month,
+    this.year = year,
+    this.description = description,
+    this.completed = completed
+  };
 
-
-  this.isWithinMonthYear = function(month = '', year = ''){
+  isWithinMonthYear(month = '', year = ''){
     month = month.toString()
     year = year.toString()
     return (this.month === month && this.year === year)    
   }
 }
 
-
-let todoList = (function() {
+const todoList = (function() {
   let idCounter = 0
   let list = []
 
   function updateProperty(title, property, newValue){
     // Updates existing properties of a specific todo object
-    let selectedTodo;
+    let todo;
 
     for(let i = 0; i < list.length; i++){
-      if(list[i].title == title){
-        selectedTodo = list[i]
-        break
+      todo = list[i]
+      if(todo.title == title){
+        todo[property] = newValue
+
+        let newObject =  new Todo(todo.title, todo.id, todo.month, todo.year, todo.description, todo.completed)
+        return newObject;
       }
     }
 
-    selectedTodo[property] = newValue
   }
 
   let listObject =  {
     addTodo: function(todoData){
       // Adds a todo object to the collection
       idCounter++
-      let newTodo = new Todo(todoData.title, idCounter, todoData.month, todoData.year, todoData.description );
-      list.push(newTodo);
-      return newTodo
+      let todo = new Todo(todoData.title, idCounter, todoData.month, todoData.year, todoData.description);
+      list.push(todo);
+
+      return new Todo(todo.title, todo.id, todo.month, todo.year, todo.description);
     },
 
     deleteTodo: function(title){
@@ -55,42 +58,54 @@ let todoList = (function() {
     getTodoById(idInput){
       // Returns a specified todo object based on its id property
       for(let i = 0; i < list.length; i ++){
-        if (list[i].id = idInput) return list[i]
+        let todo = list[i]
+        if (todo.id === idInput) {
+          return new Todo(todo.title, todo.id, todo.month, todo.year, todo.description, todo.completed)
+        }
       }
     },
 
     getList(){
       return list.map(function(todo){
-        return {
-          id: todo.id,
-          title: todo.title,
-          month: todo.month,
-          year: todo.year,
-          description: todo.description
-        }
+        return new Todo(todo.title, todo.id, todo.month, todo.year, todo.description, todo.completed)
       })
     },
 
-    updateTitle(title, newValue){
-      updateProperty(title, 'title', newValue)
-      return 
+    updateTitle(title, newTitle){
+      return updateProperty(title, 'title', newTitle)
+       
     },
 
-    updateMonth(title, newValue){
-      return updateProperty(title, 'month', newValue)
+    updateMonth(title, newMonth){
+      return updateProperty(title, 'month', newMonth)
+
     },
 
-    updateYear(title, newValue){
-      return updateProperty(title, 'year', newValue)
+    updateYear(title, newYear){
+      return updateProperty(title, 'year', newYear)
     },
 
-    updateDescription(title, newValue){
-      return updateProperty(title, 'description', newValue)
+    updateDescription(title, newDescription){
+      return updateProperty(title, 'description', newDescription)
     },
 
     completeTodo(title){
-      return this.updateProperty(title, 'completed', true)
-    }, 
+      let todo;
+
+      for (let i = 0; i < list.length; i ++){
+        todo = list[i]
+        if (todo.title === title){
+          if (todo.completed === false){
+            return updateProperty(title, 'completed', true)
+          } else {
+            return updateProperty(title, 'completed', false)
+          }
+        }
+      }
+      return undefined
+    },
+
+
   }
 
    // Initializing todoList with input data
@@ -129,7 +144,7 @@ let todoList = (function() {
 
 })()
 
-let todoManager = {
+const todoManager = {
 
   getAll: function(){
     // Returns all todo objects
@@ -173,6 +188,7 @@ let todoManager = {
     let list = todoList.getList()
 
     list.forEach(function(todo){
+
       if(todo.isWithinMonthYear(month, year) && todo.completed === true){
         output.push(todo)
       }
@@ -188,77 +204,118 @@ let todoManager = {
 
 
 
-// ---------------------------------------------
-
-
 // Tests
 
+// ---------------------------------------------
 
-//// removing specific item from list
-//console.log(todoList.getList())
-//todoList.deleteTodo("Buy Veggies")
-//console.log(todoList.getList()) // displays that 'Buy Veggies' todo is now missing from the list
+// removing specific item from list
+console.log(todoList.getList())
+todoList.deleteTodo("Buy Veggies")
+console.log(todoList.getList()) // displays that 'Buy Veggies' todo is now missing from the list
 
-//// list is private within todoList
-//console.log(todoList.list) // returns undefined
+// ---------------------------------------------
 
-//// list is only operable through getList function. todoList.getList() returns a copy of the list, not the actual list
-//let list = todoList.getList() 
-//console.log(list)
-//list[0] = "string" // reassigns first element in copied list
-//console.log(list) // first object in copied list is a string
-//console.log(todoList.getList()) // first object remains unchanged in original list
+// list is private within todoList
 
-//// new todos are given a unique id number
-//let todoData5 = {
-//  title: 'Go to the Gym',
-//  month: '',
-//  year: '',
-//  description: 'To burn off the cheat day treat',
-//};
-//todoList.addTodo(todoData5)
-//console.log(todoList.getList()) // new data is added to list, with a new and unique id number
+console.log(todoList.list) // returns undefined
 
-//// todos share the `isWithinMonthYear` method
-//let firstTodo = todoList.getList()[0]
-//console.log(firstTodo.hasOwnProperty('isWithinMonthYear')) // returns false
+// ---------------------------------------------
 
-//// `isWithinMonthYear` method returns true when month year combination of calling object matches the passed in values
-//console.log(firstTodo.isWithinMonthYear(1, 2017)) // returns true
-//console.log(firstTodo.isWithinMonthYear(5, 2017)) // returns false
+// list is only operable through getList function. todoList.getList() returns a copy of the list, not the actual list
 
-//let secondTodo = todoList.getList()[1]
-//console.log(secondTodo.isWithinMonthYear(1, 2017)) // returns false
-//console.log(secondTodo.isWithinMonthYear('', 2017)) // returns true
+let list = todoList.getList() 
+console.log(list)
+list[0] = "string" // reassigns first element in copied list
+console.log(list) // first object in copied list is a string
+console.log(todoList.getList()) // first object remains unchanged in original list
 
-//// updates existing properties of a specific todo object
-//todoList.updateProperty('Buy Milk', 'month', 2)
-//console.log(todoList.getList()[0]) // displays 'Buy Milk' todo object with month value as 2
+// ---------------------------------------------
+
+// new todos are given a unique id number
+
+let todoData5 = {
+  title: 'Go to the Gym',
+  month: '',
+  year: '',
+  description: 'To burn off the cheat day treat',
+};
+todoList.addTodo(todoData5)
+console.log(todoList.getList()) // new data is added to list, with a new and unique id number
+
+// ---------------------------------------------
+
+// todo objects do not have their own copy of `isWithinMonthYear` method
+// `isWithinMonthYear` method returns true when calling todo object has same month and year property as passed in month and year values
+
+let firstTodo = todoList.getList()[0]
+console.log(firstTodo.isWithinMonthYear(1, 2017)) // returns true
+console.log(firstTodo.isWithinMonthYear(2, 2017)) // returns false
+console.log(firstTodo.hasOwnProperty('isWithinMonthYear')) // returns false
+
+// ---------------------------------------------
+
+// updates existing properties of a specific todo object
+
+console.log(todoList.getList()[0]) // displays 'Buy Milk' todo object with month value as 1
+todoList.updateMonth('Buy Milk', '2') // updates 'Buy Milk' todo object's month property to 2
+console.log(todoList.getList()[0]) // displays 'Buy Milk' todo object with month value as 2
+todoList.updateMonth('Buy Milk', '2').month = 100000 // attempts to change month to 10000 by editing month property of returned todo object
+console.log(todoList.getList()[0]) // displays 'Buy Milk' todo object with month value still as 2
+
+// ---------------------------------------------
 
 // a todos id cannot be changed
+
 console.log(todoList.getList()[0]) // id for this object shows as 1
-todoList.getList()[0].id = 10000
+todoList.getList()[0].id = 10000 // attempt to change the id of a todo through the returned list
 console.log(todoList.getList()[0]) // id for this object remains as 1.
 
-//// returns a specified todo object based on its `id` property
-//console.log(todoList.getTodoById(2)) // returns todo list with id 2
+// ---------------------------------------------
 
-//// returns all completed todo objects
-//todoList.completeTodo('Buy Milk')
-//todoList.completeTodo('Buy Apples')
-//console.log(todoManager.getCompleted()) // returns array of todos with 'Buy Milk' and 'Buy Apples' titles
+// returns a specified todo object based on its `id` property
 
-//// returns all todo objects within a given month-year combination
-//let todoData6 = {
-//  title: 'Study JavaScript',
-//  month: '2',
-//  year: '2017',
-//  description: "I think I'm going to cry while I'm doing it, but I still have to do it",
-//};
-//todoList.addTodo(todoData6)
-//console.log(todoManager.getWithinMonthYear(2, 2017)) // returns an array with 'Buy Milk' and 'Study JavaScript' todo objects
+console.log(todoList.getTodoById(2)) // returns todo list with id 2
+console.log(todoList.getTodoById(3)) // returns todo list with id 3
 
-//// returns all completed todo objects within a given month-year combination
-//todoList.completeTodo('Study JavaScript')
-//console.log(todoList.getList())
-//console.log(todoManager.getCompletedWithinMonthYear(2, 2017)) // returns array of only 'Study JavaScript' todo object
+// ---------------------------------------------
+
+// returns all completed todo objects
+
+todoList.completeTodo('Buy Milk')
+todoList.completeTodo('Buy Apples')
+console.log(todoManager.getCompleted()) // returns array of todos with 'Buy Milk' and 'Buy Apples' titles
+
+// ---------------------------------------------
+
+// returns all todo objects within a given month-year combination
+
+let todoData6 = {
+  title: 'Study JavaScript',
+  month: '2',
+  year: '2017',
+  description: "I think I'm going to cry while I'm doing it, but I still have to do it",
+};
+
+todoList.addTodo(todoData6)
+console.log(todoList.getList())
+console.log(todoManager.getWithinMonthYear(2, 2017)) // returns an array with 'Buy Milk' and 'Study JavaScript' todo objects
+
+// ---------------------------------------------
+
+// returns all completed todo objects within a given month-year combination
+
+todoList.completeTodo('Study JavaScript')
+todoList.updateMonth('Buy Milk', '2') // updates 'Buy Milk' todo object's month property to 2
+console.log(todoManager.getCompletedWithinMonthYear(2, 2017)) // returns array of only 'Study JavaScript' todo object
+
+// ---------------------------------------------
+
+//cannot use `updateProperty` method to change properties
+
+//todoList.updateProperty('Buy Milk', 'title', 'Steal Milk') // returns error
+
+//// ---------------------------------------------
+
+// todoList.completeTodo with a title that already has a completed property value of true will be toggled back to false
+todoList.completeTodo('Study JavaScript')
+console.log(todoList.getList()) // displays 'Study JavaScript' todo with `completed` property being false
